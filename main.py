ac0f5312e0ecc89b8dc92780b2451e6c7f8af4a9
bsalsa2 <bradensalcetti@icloud.com>
2026-06-18 01:55:18 +0000

    Add CORS middleware

--- main.py
+++ main.py
@@ -13,6 +13,15 @@ import utils
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 security = HTTPBearer()
 
 # Data classes (replacing Pydantic)
